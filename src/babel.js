bddca79fafe91a42aab840e619b7342ce8492dca
Den Pol <pravosleva@uremont.com>
2020-02-03 10:07:44 +0300

    master. plugin-proposal-class-properties added

--- src/babel.js
+++ src/babel.js
@@ -3,3 +3,10 @@ async function start() {
 }
 
 start().then(() => console.log('aync is working'));
+
+// Poroposal sample:
+class Util {
+  static id = Date.now();
+}
+
+console.log('Util Id:', Util.id);
--- webpack.config.js
+++ webpack.config.js
@@ -117,6 +117,10 @@ module.exports = {
             presets: [
               '@babel/preset-env',
             ],
+            plugins: [
+              '@babel/plugin-proposal-class-properties', // class - это еще не
+              // стандарт языка (только предложение)
+            ],
           },
         },
       }
